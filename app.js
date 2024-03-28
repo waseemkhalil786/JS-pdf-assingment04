52a16cb902eadc3052bab923621cac8681dca1c9
Waseem khalil <waseemkhalilworl@gmail.com>
2024-03-27 19:29:53 -0700

    addfile in pdf 04 start

---
+++ app.js
@@ -0,0 +1,33 @@
+// start pdf 04 quostions 
+
+
+//4.1 pdf 
+
+let pdf1="wk developer";
+var pdf2="wk developer";
+const pdf3="wk developer"
+
+console.log(pdf1+ pdf2+ pdf3);
+
+//4.2 pdf
+
+//leagal variables
+
+let firstName;
+let user_age;
+let myVar;
+let _counter;
+let camelCaseVariable;
+
+//illeagal  variables
+
+
+// let one the begguest;
+// let 1234two;
+// let tHree;
+// let five-in;
+// let let = 10;
+
+//4.3 pdf
+
+let html = document.getElementById(h1).innerHTML;
